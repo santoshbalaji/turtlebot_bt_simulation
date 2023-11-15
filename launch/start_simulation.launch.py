940eb57cdd6ca5d5a53542e5af56ebbcfd1332e0
Santosh Balaji Selvaraj <selvarajs@artc.a-star.edu.sg>
2023-11-16 04:25:09 +0800

    adding basic world file

---
+++ launch/start_simulation.launch.py
@@ -0,0 +1,87 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+ 
+def generate_launch_description():
+ 
+  pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
+  launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+   
+  pkg_share = FindPackageShare(package='turtlebot_bt_simulation').find('turtlebot_bt_simulation')
+ 
+  world_file_name = 'playground.world'
+  world_path = os.path.join(pkg_share, 'world', world_file_name)
+   
+  gazebo_models_path = os.path.join(pkg_share, 'models')
+  os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+ 
+  headless = LaunchConfiguration('headless')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_simulator = LaunchConfiguration('use_simulator')
+  world = LaunchConfiguration('world')
+ 
+  declare_simulator_cmd = DeclareLaunchArgument(
+    name='headless',
+    default_value='False',
+    description='Whether to execute gzclient')
+     
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='true',
+    description='Use simulation (Gazebo) clock if true')
+ 
+  declare_use_simulator_cmd = DeclareLaunchArgument(
+    name='use_simulator',
+    default_value='True',
+    description='Whether to start the simulator')
+ 
+  declare_world_cmd = DeclareLaunchArgument(
+    name='world',
+    default_value=world_path,
+    description='Full path to the world model file to load')
+    
+  start_gazebo_server_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+    condition=IfCondition(use_simulator),
+    launch_arguments={'world': world}.items())
+ 
+  start_gazebo_client_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+    condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
+  
+  robot_state_publisher_cmd = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+          os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+      ),
+      launch_arguments={'use_sim_time': use_sim_time}.items()
+  )
+
+  spawn_turtlebot_cmd = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+          os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+      ),
+      launch_arguments={
+          'x_pose': LaunchConfiguration('x_pose', default='0.0'),
+          'y_pose': LaunchConfiguration('y_pose', default='0.0')
+      }.items()
+  )
+ 
+  ld = LaunchDescription()
+ 
+  ld.add_action(declare_simulator_cmd)
+  ld.add_action(declare_use_sim_time_cmd)
+  ld.add_action(declare_use_simulator_cmd)
+  ld.add_action(declare_world_cmd)
+ 
+  ld.add_action(start_gazebo_server_cmd)
+  ld.add_action(start_gazebo_client_cmd)
+  ld.add_action(robot_state_publisher_cmd)
+  ld.add_action(spawn_turtlebot_cmd)
+ 
+  return ld
